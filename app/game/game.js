23376c881bb9076d59a2dbba9e9e50a81c68afa3
T-rav <tmfrisinger@gmail.com>
2018-08-22 14:34:34 +0200

    Got grid rendering

--- app/game/game.js
+++ app/game/game.js
@@ -4,16 +4,16 @@ angular.module('app.game', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/game', {
     templateUrl: 'game/game.html',
-    controller: 'GameCtrl'
+    controller: 'GameController'
   });
 }])
 .controller('GameController', function($scope) {
   var player1 = 'X';
   $scope.board = {
     grid: [
-            [' ',' ',' '],
-            [' ',' ',' '],
-            [' ',' ',' ']
+            [{marker:' '},{marker:' '},{marker:' '}],
+            [{marker:' '},{marker:' '},{marker:' '}],
+            [{marker:' '},{marker:' '},{marker:' '}]
           ],
     currentPlayer: player1
     };
